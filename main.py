d3e63f258dfa281aff182db3a8c8d1805115ff72
mattbixby123 <matthew.bixby1@gmail.com>
2025-09-15 15:11:00 -0400

    Built Tool #1: Get Files Info - finished the tests.py in root as well as fixed error where dirctories outside of the working directory (ex /bin) were accessibile from within the project

--- main.py
+++ main.py
@@ -36,5 +36,5 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
-print(get_files_info("calculator"))
+print(get_files_info("calculator", "pkg"))
 # main()
