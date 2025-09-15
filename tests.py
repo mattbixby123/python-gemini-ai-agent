e03cdaaf94cfb3a4589114a02f958e156d9d11a3
mattbixby123 <matthew.bixby1@gmail.com>
2025-09-15 16:06:06 -0400

    Built Tool #3: Write file - tested

--- tests.py
+++ tests.py
@@ -1,12 +1,17 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
+from functions.write_file import write_file
 
 def main():
   working_dir = "calculator"
-  # print(get_file_content(working_dir, "lorem.txt"))
-  print(get_file_content(working_dir, "main.py"))
-  print(get_file_content(working_dir, "pkg/calculator.py"))
-  print(get_file_content(working_dir, "pkg/notexists.py"))
-  print(get_file_content(working_dir, "/bin/cat"))
+  # print(write_file(working_dir, "lorem.txt", "wait, this isn't lorem ipsum"))
+  # print(write_file(working_dir, "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
+  # print(write_file(working_dir, "/tmp/temp.txt", "this should not be allowed"))
+  # print(write_file(working_dir, "pkg2/temp.txt", "this should be allowed"))
+
+  # print(get_file_content(working_dir, "main.py"))
+  # print(get_file_content(working_dir, "pkg/calculator.py"))
+  # print(get_file_content(working_dir, "pkg/notexists.py"))
+  # print(get_file_content(working_dir, "/bin/cat"))
 
 main()
