87aadfa64a084dd83f82769f02f8e5d1e151f639
mattbixby123 <matthew.bixby1@gmail.com>
2025-09-15 15:44:49 -0400

    Built Tool #2: Get File Content - tested with ammended tests.py file

--- tests.py
+++ tests.py
@@ -1,14 +1,12 @@
 from functions.get_files_info import get_files_info
+from functions.get_file_content import get_file_content
 
 def main():
   working_dir = "calculator"
-  root_contents = get_files_info(working_dir)
-  print(root_contents)
-  pkg_contents = get_files_info(working_dir, "pkg")
-  print(pkg_contents)
-  bin_contents = get_files_info(working_dir, "/bin")
-  print(bin_contents)
-  walkup_contents = get_files_info(working_dir, "../")
-  print(walkup_contents)
+  # print(get_file_content(working_dir, "lorem.txt"))
+  print(get_file_content(working_dir, "main.py"))
+  print(get_file_content(working_dir, "pkg/calculator.py"))
+  print(get_file_content(working_dir, "pkg/notexists.py"))
+  print(get_file_content(working_dir, "/bin/cat"))
 
 main()
