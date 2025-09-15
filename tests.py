8a865f7bda5fa289a460dc87b70e8941071b0a69
mattbixby123 <matthew.bixby1@gmail.com>
2025-09-15 16:33:52 -0400

    Built Tool #4:  Run Python files - tested with various input/files

--- tests.py
+++ tests.py
@@ -1,17 +1,14 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python_file import run_python_file
 
 def main():
   working_dir = "calculator"
-  # print(write_file(working_dir, "lorem.txt", "wait, this isn't lorem ipsum"))
-  # print(write_file(working_dir, "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
-  # print(write_file(working_dir, "/tmp/temp.txt", "this should not be allowed"))
-  # print(write_file(working_dir, "pkg2/temp.txt", "this should be allowed"))
-
-  # print(get_file_content(working_dir, "main.py"))
-  # print(get_file_content(working_dir, "pkg/calculator.py"))
-  # print(get_file_content(working_dir, "pkg/notexists.py"))
-  # print(get_file_content(working_dir, "/bin/cat"))
+  # print(run_python_file(working_dir, "main.py"))
+  # print(run_python_file(working_dir, "tests.py"))
+  # print(run_python_file(working_dir, "../main.py"))
+  # print(run_python_file(working_dir, "nonexistent.py"))
+  print(run_python_file(working_dir, "main.py", ["3 + 5"]))
 
 main()
