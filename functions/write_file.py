01fd4d97b647113752ad63d3956d03e1f39634b6
mattbixby123 <matthew.bixby1@gmail.com>
2025-09-15 21:24:11 -0400

    Function declaratoins completed - ready for function call setup

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,4 +1,6 @@
 import os
+from google.genai import types
+
 
 def write_file(working_directory, file_path, content):
   abs_working_dir = os.path.abspath(working_directory)
@@ -18,3 +20,21 @@ def write_file(working_directory, file_path, content):
     return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
   except Exception as e:
     return f"Failed to write to file: {file_path}, {e}"
+
+schema_write_file = types.FunctionDeclaration(
+    name="write_file",
+    description="Overwrites an existing file or writes to a new file if it doesn't exist (and creates required parent dirs), constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "file_path": types.Schema(
+                type=types.Type.STRING,
+                description="The path to the file to write.",
+            ),
+            "contents": types.Schema(
+                type=types.Type.STRING,
+                description="The contents to write to the file as a string",
+            ),
+        },
+    ),
+)
--- main.py
+++ main.py
@@ -4,6 +4,10 @@ from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 from functions.get_files_info import schema_get_files_info
+from functions.get_file_content import schema_get_file_content
+from functions.run_python_file import schema_run_python_file
+from functions.write_file import schema_write_file
+
 
 def main():
     load_dotenv()
@@ -16,6 +20,9 @@ def main():
     When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
 
     - List files and directories
+    - Read the content of a file
+    - Write to a file (create or update)
+    - Run a Python file with optional arguments
 
     All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
     """
@@ -34,6 +41,9 @@ def main():
     available_functions = types.Tool(
         function_declarations=[
             schema_get_files_info,
+            schema_get_file_content,
+            schema_write_file,
+            schema_run_python_file,
         ]
     )
 
